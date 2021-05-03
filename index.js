9671f13178094fbbf70adb20c6d94921ae121df9
seifeldeen3388 <seifeldeen3388@gmail.com>
2021-05-03 16:28:38 +0200

    test change to code

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/health', (req, res) => {
   res.send({ status: `${appName}-health` });
 });
 
+app.get('/test', (req, res) => {
+  res.send({ status: `${appName}-test` });
+});
+
+
 // app.get('/deep-health', async (req, res) => {
 //   try {
 //     const response = {};
